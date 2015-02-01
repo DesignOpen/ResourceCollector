fb90934257e44a8f5c509e25092e579ad10fb336
Christian Mata <christian.m.mata@gmail.com>
2015-02-01 09:31:09 -0800

    Made the basic form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var categories = ["benefits", "blog", "challenges", "community", "design", "education", "example", "opendesign", "source"];
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { categories: categories });
 });
 
 module.exports = router;
