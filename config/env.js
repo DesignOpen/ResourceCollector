3e17b7984c78ec10ad8c741525bd180c55c64465
Jonathan Lau <jon@jon.sg>
2015-02-01 10:33:24 -0800

    Add API for posting issue to Github

---
+++ config/env.js
@@ -0,0 +1,4 @@
+module.exports = {
+	github_key: process.env.github_key,
+	github_repo: process.env.github_repo
+};
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var github = require('octonode');
+var env = require('../config/env');
 
 /* Process new resource form */
 router.post('/resource', function(req, res) {
-  console.log(req.body);
-  // STUB: Process form
+  var title = req.body.title;
+  var url = req.body.URL;
+  var twitter = req.body.twitter;
+  var description = req.body.description;
+  var category = req.body.category;
+
+  // STUB: Validate fields
+
+  var body = "URL: " + url + "\n";
+  body = body + "Category: " + category + "\n";
+  body = body + "Description: " + description + "\n\n";
+  body = body + "Submitted by: http://twitter.com/" + twitter.replace(/@/g, '') + "\n";
+
+  var client = github.client(env.github_key);
+  var ghrepo = client.repo(env.github_repo);
+
+  ghrepo.issue({
+    "title": "Add Resource: " + title,
+    "body": body,
+    "labels": ["resource"]
+  }, function(err, res) {
+    console.log(err, res);
+  });
+
+  // STUB: Send proper JSON response
+
+  res.send('success');
 });
 
 module.exports = router;
