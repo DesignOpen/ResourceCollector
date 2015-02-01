7ca147b8e2859818343af7df9645e36f7dc01e1a
Jonathan Lau <jon@jon.sg>
2015-02-01 11:25:31 -0800

    Add input validation

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,61 @@
 var express = require('express');
 var router = express.Router();
 var github = require('octonode');
+var validator = require('validator');
 var env = require('../config/env');
 
 /* Process new resource form */
 router.post('/resource', function(req, res) {
   var title = req.body.title;
   var url = req.body.URL;
-  var twitter = req.body.twitter;
+  var twitter = req.body.twitter.replace(/@/g, '');
   var description = req.body.description;
   var category = req.body.category;
 
-  // STUB: Validate fields
+  if (validator.isNull(title)) {
+  	res.json({
+  		success: false,
+  		error: 'Please enter a title.'
+  	});
+  	return;
+  }
+
+  if (!validator.isURL(url, {require_protocol: true})) {
+  	res.json({
+  		success: false,
+  		error: 'Please enter a valid URL.'
+  	});
+  	return;
+  }
+
+  if (validator.isNull(twitter)) {
+  	res.json({
+  		success: false,
+  		error: 'Please enter your Twitter handle.'
+  	});
+  	return;
+  }
+
+  if (validator.isNull(description)) {
+  	res.json({
+  		success: false,
+  		error: 'Please enter a description.'
+  	});
+  	return;
+  }
+
+  if (validator.isNull(category)) {
+  	res.json({
+  		success: false,
+  		error: 'Please choose a category.'
+  	});
+  	return;
+  }
 
   var body = "URL: " + url + "\n";
   body = body + "Category: " + category + "\n";
   body = body + "Description: " + description + "\n\n";
-  body = body + "Submitted by: http://twitter.com/" + twitter.replace(/@/g, '') + "\n";
+  body = body + "Submitted by: http://twitter.com/" + twitter + "\n";
 
   var client = github.client(env.github_key);
   var ghrepo = client.repo(env.github_repo);
@@ -25,13 +64,19 @@ router.post('/resource', function(req, res) {
     "title": "Add Resource: " + title,
     "body": body,
     "labels": ["resource"]
-  }, function(err, res) {
-    console.log(err, res);
+  }, function(err, response) {
+  	if (err) {
+	  	res.json({
+	  		success: false,
+	  		error: 'Unable to process your entry, please try again later.'
+	  	});
+  	}
+  	res.json({
+  		success: true,
+  		url: response.url,
+  		issue: response.number
+  	});
   });
-
-  // STUB: Send proper JSON response
-
-  res.send('success');
 });
 
 module.exports = router;
