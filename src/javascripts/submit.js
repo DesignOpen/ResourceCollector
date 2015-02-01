bcd5526a056d5df76ce71156d6fb343478463961
Christian Mata <christian.m.mata@gmail.com>
2015-02-01 14:44:36 -0800

    Made simple form feedback banner

--- src/javascripts/submit.js
+++ src/javascripts/submit.js
@@ -4,13 +4,67 @@
 */
 var ajax = require('component-ajax');
 
-function submitForm(){
-	ajax.ajax('/api/resource', {
-		success: function(result){
-      console.log('success!', result);
-    },
+window.submitForm = function(){
+  //create the data object from the form
+  var data = {
+    'title': document.forms['submitResource']['title'].value,
+    'URL': document.forms['submitResource']['URL'].value,
+    'twitter': document.forms['submitResource']['twitter'].value, 
+    'description': document.forms['submitResource']['description'].value, 
+    'category': document.forms['submitResource']['category'].value
+  }; 
+
+  //make the AJAX call to the server
+  ajax({
+    type: 'POST', 
+    url: '/api/resource',
+    data: data, 
+    success: function(result){
+      console.log('Success!', result);
+      displayBanner(result, true);
+    }, 
     error: function(err){
-      console.log('err:  ', err);
+      console.log('Error', err);
+      console.log(err.response);
+      displayBanner(err);
+      //Once error is received, add simple banner to the form
     }
   });
+
+  return false; 
+}
+
+/*
+* Method for displaying a banner with the status of the 
+* AJAX post. Automatically sets timeout for the banner after 5 seconds
+*
+* @param {Object} res - Response from the server
+* @param {boolean} success - indicator variable if the response was successful
+*/
+var displayBanner = function(res, success){
+  var msg = document.createElement('p');
+  msg.id = 'bannerText';
+  if(!success){ //its an error
+    var node = document.createTextNode('Error: ' + JSON.parse(res.response).error);
+  }
+  else{ //success
+    var response = JSON.parse(res); //have the Issue & the URL
+    var node = document.createTextNode('Success! Created Issue #' + response.issue + ' URL: ' + response.url)
+  }
+  msg.appendChild(node);
+
+  var element = document.getElementById('banner');
+  element.appendChild(msg);
+  removeBanner();
+}
+
+/*
+* Method for removing banner from the DOM.
+*
+*/
+var removeBanner = function(){
+  setTimeout(function(){
+    var msg = document.getElementById('bannerText');
+    msg.parentNode.removeChild(msg);
+  }, 5000); 
 }
