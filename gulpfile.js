ab593c8665512b127e61ac6c6ea5619426a9d50a
Christian Mata <christian.m.mata@gmail.com>
2015-02-07 23:05:08 -0500

    updated gulp file tasks and package scripts for heroku deployment

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,20 @@ var uglify = require('gulp-uglify');
 var replace = require('gulp-replace');
 
 //gulp task for production
-gulp.task('prod', function(){
-	return gulp.src(['bookmarklet.js']) //only this file 
+gulp.task('prodBookmarklet', function(){
+	return gulp.src('src/javascripts/bookmarklet.js') //only this file 
 	.pipe(replace('localhost:4000/', 'http://osdrc.herokuapp.com/')) //replace the string
 	.pipe(uglify()) //uglify result
 	.pipe(gulp.dest('public/javascripts')) //pipe into public js folder
 });
 
 //gulp task for dev
-gulp.task('dev', function(){
-	return gulp.src(['bookmarklet.js']) //only this file 
+gulp.task('devBookmarklet', function(){
+	return gulp.src('src/javascripts/bookmarklet.js') //only this file 
 	.pipe(uglify()) //uglify result
 	.pipe(gulp.dest('public/javascripts')) //pipe into public js folder
 })
 
-gulp.task('default', ['dev']);
+gulp.task('default', ['devBookmarklet']);
+
+gulp.task('prod', ['prodBookmarklet']);
