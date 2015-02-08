f4cc867a5b9bfc3f8e2b5b6300ddc0bdbfe87e8d
Christian Mata <christian.m.mata@gmail.com>
2015-02-07 23:13:31 -0500

    Updated the dest directory. Made it slightly shorter

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,14 @@ gulp.task('prodBookmarklet', function(){
 	return gulp.src('src/javascripts/bookmarklet.js') //only this file 
 	.pipe(replace('localhost:4000/', 'http://osdrc.herokuapp.com/')) //replace the string
 	.pipe(uglify()) //uglify result
-	.pipe(gulp.dest('public/javascripts')) //pipe into public js folder
+	.pipe(gulp.dest('public/js')) //pipe into public js folder
 });
 
 //gulp task for dev
 gulp.task('devBookmarklet', function(){
 	return gulp.src('src/javascripts/bookmarklet.js') //only this file 
 	.pipe(uglify()) //uglify result
-	.pipe(gulp.dest('public/javascripts')) //pipe into public js folder
+	.pipe(gulp.dest('public/js')) //pipe into public js folder
 })
 
 gulp.task('default', ['devBookmarklet']);
