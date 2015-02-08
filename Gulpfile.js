24dfbc80395bf5c24ce71a7f8638a5672028c5b0
garthdb <garthdb@gmail.com>
2015-02-07 22:43:51 -0700

    adding browsersync and gulp to generate static files

---
+++ Gulpfile.js
@@ -0,0 +1,75 @@
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+var prefix = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var filter = require('gulp-filter');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer  = require('vinyl-buffer');
+var path = require('path');
+var uglify = require('gulp-uglifyjs');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('css', function() {
+  return gulp.src('src/stylesheets/*.styl').pipe(stylus({
+    compress: true,
+    sourcemap: {
+      inline: true,
+      sourceRoot: '.',
+      basePath: 'public/stylesheets'
+    }
+  })).pipe(sourcemaps.init({
+    loadMaps: true
+  }))
+  .pipe(prefix("> 1%"))
+  .pipe(sourcemaps.write('./', {}))
+  .pipe(gulp.dest('public/stylesheets'))
+  .pipe(filter('**/*.css'));
+});
+
+gulp.task('js', function() {
+  var bundler = browserify({
+    entries: ['./src/javascripts/main.js'],
+    debug: true,
+    transform: 'reactify'
+  });
+
+  var bundle = function() {
+    return bundler
+      .bundle()
+      .pipe(source('bundle.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        // .pipe(uglify())
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./public/javascripts/'));
+      // .pipe(reload({stream: true}));
+  };
+
+  return bundle();
+});
+
+gulp.task('browser-sync', ['nodemon'], function() {
+  serve.init(null, {
+    proxy: "http://localhost:3000",
+    files: ["public/**/*.*"],
+    browser: "google chrome",
+    port: 7000,
+  });
+});
+
+gulp.task('nodemon', function (cb) {
+  return nodemon({
+    script: 'bin/www'
+  }).on('start', function () {
+    cb();
+  });
+});
+
+gulp.task('default', ['css', 'js', 'browser-sync'], function() {
+  gulp.watch('src/stylesheets/*.styl', ['css']);
+  gulp.watch('src/javascripts/*.js', ['js']);
+});
--- app.js
+++ app.js
@@ -1,37 +1,18 @@
 var express = require('express');
-var stylus = require("stylus");
-var autoprefixer = require('autoprefixer-stylus');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var browserify = require('browserify-middleware');
-var reactify    = require('reactify');
 
 var routes = require('./routes/index');
 var api = require('./routes/api');
-
-browserify.settings({
-  transform: ['reactify']
-});
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-// Stylus middleware
-app.use(stylus.middleware(
-  { src: __dirname + '/public'
-  , compile: function (str, path) {
-    return stylus(str)
-    .use(autoprefixer())
-    .set('filename', path)
-  }
-}
-));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -43,8 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/api', api);
-
-app.get('/javascripts/main.js', browserify('./src/javascripts/bundle.js'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
