5d8b82a8b222f760ad7429f6ffbcb7830c59cb52
Jonathan Lau <jon@jon.sg>
2015-02-09 19:21:06 +0800

    Pull categories from remote jsonp

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-var categories = ["benefits", "blog", "challenges", "community", "design", "education", "example", "opendesign", "source"];
-
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', {
   	title: req.query.title || '',
   	url: req.query.url || '',
-  	twitter: req.query.twitter || '',
-  	categories: categories 
+  	twitter: req.query.twitter || ''
   });
 });
 
