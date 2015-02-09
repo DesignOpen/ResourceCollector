d2e56c9c1ded6c7f531edb97d665dcc9b799fbb5
garthdb <garthdb@gmail.com>
2015-02-09 16:36:02 -0700

    working on getting gulp and express to play nice with browsersync

--- src/javascripts/components/FormComponent.js
+++ src/javascripts/components/FormComponent.js
@@ -4,7 +4,7 @@
 
 var React = require('react')
 
-var form = React.createClass({
+var FormComponent = React.createClass({
   render: function () {
     return (
       <div className="container">
@@ -14,15 +14,15 @@ var form = React.createClass({
           <form id="submitResource" onsubmit="return submitForm()">
             <div className="form-item">
               <label htmlFor="titleInput">Title</label>
-              <input id="titleInput" type="text" name="title" value="<%= title %>" required>
+              <input id="titleInput" type="text" name="title" value="<%= title %>" required/>
             </div>
             <div className="form-item">
               <label htmlFor="urlInput">URL</label>
-              <input id="urlInput" type="url" name="URL" value="<%= url %>" required>
+              <input id="urlInput" type="url" name="URL" value="<%= url %>" required/>
             </div>
             <div className="form-item">
               <label htmlFor="twitterInput">Twitter</label>
-              <input id="twitterInput" type="text" name="twitter" value="<%= twitter %>" required>
+              <input id="twitterInput" type="text" name="twitter" value="<%= twitter %>" required/>
             </div>
             <div className="form-item">
               <label htmlFor="descriptionInput">Description</label>
@@ -32,12 +32,12 @@ var form = React.createClass({
               <label htmlFor="categoryInput">Category</label>
               <select id="categoryInput" name="category">
                 {this.props.categories.map(function(category) {
-                  return <option>{category}</option>
+                  return <option value={category}>{category}</option>
                 })}
               </select>
             </div>
             <div className="form-item">
-              <input type="submit" value="Submit Resource" id="resourceSubmit">
+              <input type="submit" value="Submit Resource" id="resourceSubmit"/>
             </div>
           </form>
         </div>
@@ -46,3 +46,5 @@ var form = React.createClass({
   }
 
 });
+
+module.exports = FormComponent;
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,36 +1,51 @@
+/**
+* @jsx React.DOM
+*/
+'use strict';
+
+var React = require('react');
+window.React = React; // export for http://fb.me/react-devtools
+
+var FormComponent = require('./components/FormComponent');
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  React.render(<FormComponent categories={["philosophy","sources","redesigns","inspiration","tutorials",]}/>, document.body);
+}, false);
+
 /*
 * Method to make an AJAX call to the API.
 *
 */
-var ajax = require('component-ajax');
-
-window.submitForm = function(){
-  //create the data object from the form
-  var data = {
-    'title': document.forms['submitResource']['title'].value,
-    'URL': document.forms['submitResource']['URL'].value,
-    'twitter': document.forms['submitResource']['twitter'].value, 
-    'description': document.forms['submitResource']['description'].value, 
-    'category': document.forms['submitResource']['category'].value
-  };
-
-  document.getElementById('resourceSubmit').disabled = true;
-
-  //make the AJAX call to the server
-  ajax({
-    type: 'POST', 
-    url: '/api/resource',
-    data: data,
-    success: function(result){
-      var url = JSON.parse(result).url;
-      document.getElementById('banner').innerHTML = 'Thank you for your contribution. Your submission has been submitted for review. Follow up on your submission: <a href="' + url + '" target="_blank">' + url + '</a>';
-      document.getElementById('resourceForm').innerHTML = '';
-    }, 
-    error: function(err){
-      document.getElementById('banner').innerHTML = JSON.parse(err.response).error || 'Unable to process your entry, please try again later.';
-      document.getElementById('resourceSubmit').disabled = false;
-    }
-  });
-
-  return false; 
-}
+// var ajax = require('component-ajax');
+//
+// window.submitForm = function(){
+//   //create the data object from the form
+//   var data = {
+//     'title': document.forms['submitResource']['title'].value,
+//     'URL': document.forms['submitResource']['URL'].value,
+//     'twitter': document.forms['submitResource']['twitter'].value,
+//     'description': document.forms['submitResource']['description'].value,
+//     'category': document.forms['submitResource']['category'].value
+//   };
+//
+//   document.getElementById('resourceSubmit').disabled = true;
+//
+//   //make the AJAX call to the server
+//   ajax({
+//     type: 'POST',
+//     url: '/api/resource',
+//     data: data,
+//     success: function(result){
+//       var url = JSON.parse(result).url;
+//       document.getElementById('banner').innerHTML = 'Thank you for your contribution. Your submission has been submitted for review. Follow up on your submission: <a href="' + url + '" target="_blank">' + url + '</a>';
+//       document.getElementById('resourceForm').innerHTML = '';
+//     },
+//     error: function(err){
+//       document.getElementById('banner').innerHTML = JSON.parse(err.response).error || 'Unable to process your entry, please try again later.';
+//       document.getElementById('resourceSubmit').disabled = false;
+//     }
+//   });
+//
+//   return false;
+// }
