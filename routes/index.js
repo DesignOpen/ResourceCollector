e32e88ba5acf9fa1b687f287acc6cf490cd5cb24
Christian Mata <christian.m.mata@gmail.com>
2015-02-10 15:12:43 -0500

    Removed old bookmarklet routes

--- routes/index.js
+++ routes/index.js
@@ -13,11 +13,4 @@ router.get('/', function(req, res) {
   });
 });
 
-/* GET bookmarklet JS. */
-router.get('/bookmarklet.js', function(req, res) {
-	res.render('bookmarklet-js', {
-		host: req.headers.host
-	});
-});
-
 module.exports = router;
