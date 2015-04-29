fbfa9762292af25c1f33f712015dded3ec4bb6cb
Christian Mata <christian.m.mata@gmail.com>
2015-04-29 19:43:00 -0400

    Made basic tests to be built off of later

--- test/client/test-form.js
+++ test/client/test-form.js
@@ -6,10 +6,11 @@ var React = require('react');
 var FormComponent = require('../../src/javascripts/components/FormComponent');
 
 var container = document.getElementById('test-container');
+var categories = ["", "philosophy", "sources", "redesigns", "inspiration", "tutorials"];
+var queryInput = {url: "http://www.designopen.org", title: "Design Open | Home", twitter: "__christianmata"};
 
-describe('Form tests', function(){
+describe('Blank Form tests', function(){
   before(function(){
-    var categories = ["", "philosophy", "sources", "redesigns", "inspiration", "tutorials"];
     this.form = React.render(<FormComponent categories={categories} query={{}}/>, container);
   });
 
@@ -43,10 +44,66 @@ describe('Form tests', function(){
       expect(el).to.exist;
       expect(el.value).to.be.empty;
     });
+    it('should have all the category options', function(){
+      var current = [];
+      [].forEach.call(document.querySelectorAll('#categoryInput option'), function(elm){
+        current.push(elm.value);
+      });
+      expect(current).to.eql(categories);
+    })
     it('should have a submit button', function(){
       var el = document.getElementById('resourceSubmit');
       expect(el).to.exist;
     });
   });
 
+})
+
+describe('Query Form tests', function(){
+  before(function(){
+    this.form = React.render(<FormComponent categories={categories} query={queryInput}/>, container);
+  });
+
+  after(function(){
+    React.unmountComponentAtNode(container);
+  });
+
+  describe('Initialization', function(){
+    it('should have a filled Title input', function(){
+      var el = document.getElementById('titleInput');
+      expect(el).to.exist;
+      expect(el.value).to.eql(queryInput.title);
+    });
+    it('should have an filled URL input', function(){
+      var el = document.getElementById('urlInput');
+      expect(el).to.exist;
+      expect(el.value).to.eql(queryInput.url);
+    });
+    it('should have an filled Twitter input', function(){
+      var el = document.getElementById('twitterInput');
+      expect(el).to.exist;
+      expect(el.value).to.eql(queryInput.twitter);
+    });
+    it('should have an empty Description input', function(){
+      var el = document.getElementById('descriptionInput');
+      expect(el).to.exist;
+      expect(el.value).to.be.empty;
+    });
+    it('should have an empty Category input', function(){
+      var el = document.getElementById('categoryInput');
+      expect(el).to.exist;
+      expect(el.value).to.be.empty;
+    });
+    it('should have all the category options', function(){
+      var current = [];
+      [].forEach.call(document.querySelectorAll('#categoryInput option'), function(elm){
+        current.push(elm.value);
+      });
+      expect(current).to.eql(categories);
+    })
+    it('should have a submit button', function(){
+      var el = document.getElementById('resourceSubmit');
+      expect(el).to.exist;
+    });
+  });
 })
--- test/server/api.js
+++ test/server/api.js
@@ -4,17 +4,8 @@ var request = require('supertest');
 
 describe("simple test suite", function() {
   describe("constructor", function() {
-    var thing = {
-      name: 'Ben',
-      age: 10,
-      occupation: 'student'
-    }
-    it("should have a name", function() {
-      expect(thing.name).to.equal("Ben");
-    });
-
-    it("should have an age", function() {
-      expect(thing.age).to.equal(10);
+    it("should have be true", function() {
+      expect(true).to.be.true;
     });
   });
 });
